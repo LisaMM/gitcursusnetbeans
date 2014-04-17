6f5547185839bb88c5bc48f1001b7601cd10513b
Lisa <lisa.m.m.steendam@gmail.com>
2014-04-17 10:38:07 +0200

    hallo

---
+++ src/be/vdab/git/GitCursusMain.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package be.vdab.git;
+
+/**
+ *
+ * @author dev24
+ */
+public class GitCursusMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hallo");
+    }
+    
+}
